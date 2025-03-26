7f4a3cf92882f219a0badc407c8b9a741fe76b52
Виталий Овсянников <its.vint@ya.ru>
2025-03-26 16:54:18 +0300

    [Init] Project 5: Word Scramble

---
+++ 5.WordScramble/WordScramble/IBViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IBViewController.swift
+//  WordScramble
+//
+//  Created by Виталий Овсянников on 26.03.2025.
+//
+
+import UIKit
+
+class IBViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
