7cfb68dcb74419d5f0f847de5b3b82b9af6a391d
Виталий Овсянников <its.vint@ya.ru>
2025-03-26 13:49:49 +0300

    [Init] Project 2: Guess the Flag

---
+++ 2.GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Виталий Овсянников on 26.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
