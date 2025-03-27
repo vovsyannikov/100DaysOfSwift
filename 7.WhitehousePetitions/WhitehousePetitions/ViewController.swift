8a167c083bf4f2a5d623794a295c21a28062a70e
Виталий Овсянников <its.vint@ya.ru>
2025-03-27 17:54:16 +0300

    [Init] Project 7: Whitehouse Petitions

---
+++ 7.WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Виталий Овсянников on 27.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
