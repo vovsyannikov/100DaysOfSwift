b47f8464f4c8fea5c34e90ec6959714b69152f88
Виталий Овсянников <its.vint@ya.ru>
2025-03-31 16:27:46 +0300

    [Init] Project 11: Pachinko

---
+++ 11.Pachinko/Pachinko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pachinko
+//
+//  Created by Виталий Овсянников on 31.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
