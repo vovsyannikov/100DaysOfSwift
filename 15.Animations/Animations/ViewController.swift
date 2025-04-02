743fe9b60a0cd5ef2cfaffdbde9c0346cd88b5ad
Виталий Овсянников <its.vint@ya.ru>
2025-04-02 21:46:47 +0300

    [Init] Project 15: Animations

---
+++ 15.Animations/Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Виталий Овсянников on 02.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
