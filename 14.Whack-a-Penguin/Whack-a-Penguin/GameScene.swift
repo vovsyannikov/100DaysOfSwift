668e9862d343fff2bb639d76edbf01533b42fc47
Виталий Овсянников <its.vint@ya.ru>
2025-04-02 20:08:05 +0300

    [Init] Project 14: Whack-a-Penguin

---
+++ 14.Whack-a-Penguin/Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by Виталий Овсянников on 02.04.2025.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+	override func didMove(to view: SKView) {
+
+	}
+
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+	}
+}
