ed35c60be59fab3026d3c5103f01e338ba208aa3
Виталий Овсянников <its.vint@ya.ru>
2025-04-13 17:56:14 +0300

    [Init] Project 17: Space Race

---
+++ 17.SpaceRace/SpaceRace/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by Виталий Овсянников on 13.04.2025.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+	override func didMove(to view: SKView) {
+
+	}
+
+	override func update(_ currentTime: TimeInterval) {
+		
+	}
+}
