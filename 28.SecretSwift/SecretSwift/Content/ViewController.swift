242cfab5ae867db07b4bb5bac11d5c3960334b17
Виталий Овсянников <its.vint@ya.ru>
2025-04-13 22:38:15 +0300

    [Init] Project 28: Secret Swift

---
+++ 28.SecretSwift/SecretSwift/Content/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecretSwift
+//
+//  Created by Виталий Овсянников on 13.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
